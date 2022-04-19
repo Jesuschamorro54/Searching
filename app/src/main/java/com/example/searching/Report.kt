a15f61576552b5523aeee799dfefea9ccfa0f4d3
ofelia123 <ofelialondonog@gmail.com>
2022-04-18 19:37:11 -0500

    Cambio de color de la barra

--- app/src/main/java/com/example/searching/Report.kt
+++ app/src/main/java/com/example/searching/Report.kt
@@ -1,4 +1,11 @@
 package com.example.searching
 
-class Report {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Report : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.report)
+    }
 }
