f1979724889d3e033ec6213bc5e3534f008f73bc
Juanavpc <juanis.pineros48@gmail.com>
2022-04-19 00:22:24 -0500

    Primeros TextView de los chats

---
+++ app/src/main/java/com/example/searching/activity_chat_general.kt
@@ -0,0 +1,11 @@
+package com.example.searching
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_chat_general : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_general)
+    }
+}
