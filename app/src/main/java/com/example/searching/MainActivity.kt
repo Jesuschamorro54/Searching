7cbd32a42def1158c1b2b81bd0b78e9b780d806c
Jesus chamorro <jesusfeli54@gmail.com>
2022-04-24 18:22:47 -0500

    Google Maps API integration and change of views from menu bottom

--- app/src/main/java/com/example/searching/MainActivity.kt
+++ app/src/main/java/com/example/searching/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.searching
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        //createMapFragment()
+        val mapView = MapViewFragment()
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> {
+                    setCurrentFragment(mapView)  // Aquí se carga la vista que se quiere
+                    true
+                }
+                R.id.nav_add -> {
+                    setCurrentFragment(mapView)
+                    true
+                }
+                R.id.nav_messages -> {
+                    setCurrentFragment(mapView)
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.containerView, fragment)
+            commit()
+        }
+    }
+
+    private fun createMapFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.fragmentMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
     }
 }
