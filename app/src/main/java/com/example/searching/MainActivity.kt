ea98cddaead4232fb3c3745416850ebb5e6bab75
Laura Cerra <lauran-cerraq@unilibre.edu.co>
2022-05-01 10:56:12 -0500

    vista listas perdidos las imagenes no salen y no se la respuesta a eso

--- app/src/main/java/com/example/searching/MainActivity.kt
+++ app/src/main/java/com/example/searching/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.searching
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val boton=findViewById<Button>(R.id.boton_iniciar)
+        boton.setOnClickListener{
+            val mainactivityperdidos=Intent(this, MainActivityPerdidos::class.java)
+            startActivity(mainactivityperdidos)
+        }
+
     }
 }
